c8f375de90ddeaa647e541ff06537d6bfad8af15
MikhailMilitchuk <camwolfen@gmail.com>
2024-08-26 06:53:24 +0300

    Delete ViewModel directory

--- ViewModel/EventViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CalendarMvcWebApplication.ViewModel
-{
-    public class EventViewModel
-    {
-        public DateTime Date { get; set; }
-        public string Name { get; set; }
-    }
-}
