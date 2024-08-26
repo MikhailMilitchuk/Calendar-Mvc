bbaf3572a6e255682133c917ef48bb1391deb243
MikhailMilitchuk <camwolfen@gmail.com>
2024-08-26 06:53:51 +0300

    Delete Models directory

--- Models/Event.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CalendarMvcWebApplication.Models
-{
-    public class Event
-    {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public string Name { get; set; }
-    }
-}
