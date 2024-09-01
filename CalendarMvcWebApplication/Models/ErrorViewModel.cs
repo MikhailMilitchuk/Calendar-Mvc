2f65bb274eb8350cb1a141b0ef9da2d14b2feb49
MikhailMilitchuk <camwolfen@gmail.com>
2024-09-02 01:12:51 +0300

    Delete CalendarMvcWebApplication directory

--- CalendarMvcWebApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CalendarMvcWebApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- CalendarMvcWebApplication/ViewModel/CalendarViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CalendarMvcWebApplication.ViewModel
-{
-    public class CalendarViewModel
-    {
-        public DateTime Date { get; set; }
-        public DayViewModel[,] Days { get; set; }
-    }
-}
